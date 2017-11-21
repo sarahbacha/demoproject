f1c7f76aed94a41b668c3c43b420d649d83f7da1
Sarah Bacha <sarah@cedrusco.com>
2017-11-21 17:42:22 +0200

    CZAR Commit

---
+++ demoapi/R1/controllers/v1/Customer.js
@@ -0,0 +1,16 @@
+'use strict';
+var url = require('url');
+var Customer = require('./CustomerService');
+
+module.exports.getCustomer = function getCustomer (req, res, next) {
+    Customer.getCustomer (req.swagger.params, res, next);
+};
+
+module.exports.postCustomer = function postCustomer (req, res, next) {
+    Customer.postCustomer (req.swagger.params, res, next);
+};
+
+module.exports.patchCustomer = function patchCustomer (req, res, next) {
+    Customer.patchCustomer (req.swagger.params, res, next);
+};
+
